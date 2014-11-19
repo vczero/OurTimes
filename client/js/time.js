1f8060c21e02a12d485d054a18b0a914d3ccaea1
wlh <wlhmyit@126.com>
2014-11-19 17:06:15 +0800

    add Util

--- client/js/time.js
+++
@@ -1,9 +0,0 @@
-var Time = (function(date){
-	var time  = new Date(date);
-	return time.getFullYear() + '-'  +
-	       (parseInt(time.getMonth()) + 1) + '-' +
-	       time.getDate() + ' ' +
-	       time.getHours() + ':' +
-	       time.getMinutes() + ':' +
-	       time.getSeconds();
-})();
