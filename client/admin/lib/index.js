ee86e57c8a6d351455813b5619dd81a9248038e0
wlh <wlhmyit@126.com>
2014-11-20 00:01:35 +0800

    add admin UI

--- client/admin/lib/index.js
+++ client/admin/lib/index.js
@@ -19,5 +19,21 @@ app.controller('UserController', function($scope, $http){
 			obj[i].time = Util.timeFormat(obj[i].time);		}
 		$scope.datas = obj;
 	});
-	
 });
+
+app.controller('LoginController', function($scope, $http) {
+    if (document.cookie) {
+        var userid = document.cookie.split(';')[1].split('=')[1];
+        if (userid && document.cookie.indexOf('userid=') > -1) {
+            $http.get('http://127.0.0.1:3000/user/get?userid=' + userid).success(function(data) {
+                if (data.status) {
+                    $scope.user = {
+                        name: data.nickname || data.email
+                    };
+                }
+            });
+        }
+    }
+});
+
+
