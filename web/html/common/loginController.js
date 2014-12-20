e04a9f876f867c92440b6fc531675a29a17cd912
wlh <wlhmyit@126.com>
2014-12-20 23:58:55 +0800

    del html

--- web/html/common/loginController.js
+++
@@ -1,8 +0,0 @@
-//获取用户的cookie，显示欢迎信息
-app.controller('LoginController', function($scope) {
-    if (userObj) {
-         $scope.user = {
-            name: userObj.realname || userObj.nickname || userObj.email
-         }
-    }
-});
